ab6ab5a84b109cba55183055ca6dbf179e8a1788
yetingmao <yetingmao@163.com>
2019-12-11 16:29:04 +0800

    增加入口文件

---
+++ bin/main.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+/*
+ * @Description: 入口文件
+ * @Autor: yetm
+ * @Date: 2019-12-11 14:32:12
+ * @LastEditors: yetm
+ * @LastEditTime: 2019-12-11 15:29:55
+ */
+const { resolve } = require("path");
+const res = command => resolve(__dirname, "../commands/", command);
+const program = require("commander");
+const package = require("../package.json");
+
+program
+    .version(package.version);
+
+program
+    .usage("<command>"); //自定义在帮助信息第一行中显示的命令使用描述(description)
+
+
+program
+    .command("init")
+    .description("create a project")
+    .alias("i")
+    .action(() => {
+        require(res("init"))
+    });
+
+program.parse(process.argv);
+
+// 判断终端上输入出来bin中的命令是否还有其他值，如果没有终端会直接输出help
+
+if (typeof process.argv === "undefined" || process.argv.length === 0) {
+    program.help();
+}
