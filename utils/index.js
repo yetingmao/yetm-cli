85a9ace01abd99fb5b7137358d8ededaa99360a1
yetingmao <yetingmao@163.com>
2019-12-13 15:11:58 +0800

    修改名 字

--- utils/index.js
+++ utils/index.js
@@ -3,12 +3,23 @@
  * @Autor: yetm
  * @Date: 2019-12-11 17:21:31
  * @LastEditors: yetm
- * @LastEditTime: 2019-12-13 11:26:21
+ * @LastEditTime: 2019-12-13 15:10:10
  */
 const download = require("download-git-repo");
 
 function clone(frame, projectName, fc) {
-    download(`direct:https://github.com/yetingmao/${frame}`, projectName, { clone: true }, (err) => {
+    let repository;
+    switch (frame) {
+        case "react-antd-dva":
+            repository = "umi_dva"
+            break;
+        case "vue-viewdesign-vuex":
+            repository = "vue_iview_vuex"
+            break;
+        default:
+            break;
+    }
+    download(`direct:https://github.com/yetingmao/${repository}`, projectName, { clone: true }, (err) => {
         fc(err);
     });
 }
